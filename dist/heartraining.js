76b3098a7151fa0dc2f9407826af9eaca003efb8
dung.man <dtmg307@gmail.com>
2024-03-06 09:22:04 +0700

    v5

---
+++ dist/heartraining.js
@@ -0,0 +1,26 @@
+function createHeart() {
+    const heart = document.createElement("div");
+    heart.classList.add("heart-raining");
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 7 + "s";
+
+    // Tạo thẻ <img> và đặt đường dẫn của ảnh động
+    const img = document.createElement("img");
+    img.src = "../image/heart-beating.gif";
+
+    const scaleRatio = Math.random() * (0.15 - 0.05) + 0.05;
+    img.style.transform = "scale(" + scaleRatio + ")";
+
+    // Thêm thẻ <img> vào thẻ div (heart)
+    heart.appendChild(img);
+
+    // heart.innerText = '💗';
+    const parent = document.body;
+    parent?.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 10000);
+}
+setInterval(createHeart, 500);
